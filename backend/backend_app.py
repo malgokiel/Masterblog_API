8fb254b0c2f63f9c9d0a8a2e7866eecf358c6338
malgorzata <libronika@gmail.com>
2025-03-06 16:26:21 +0100

    post, delete, put, search methods implemented

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -9,10 +9,85 @@ POSTS = [
     {"id": 2, "title": "Second post", "content": "This is the second post."},
 ]
 
+def validate_post_data(data):
+    if not data['title'] or not data['content']:
+        return False
+    return True
 
-@app.route('/api/posts', methods=['GET'])
+@app.route('/api/posts', methods=['GET', 'POST'])
 def get_posts():
-    return jsonify(POSTS)
+    if request.method == 'POST':
+
+        new_post = request.get_json()
+        if not validate_post_data(new_post):
+            return jsonify({"error": "Invalid post title or content"}), 400
+        new_id = max(post['id'] for post in POSTS) + 1
+        new_post['id'] = new_id
+
+        POSTS.append(new_post)
+
+        return jsonify(new_post), 201
+    else:
+        title = request.args.get('title')
+        content = request.args.get('content')
+
+        if title and content:
+            filtered_posts = [post for post in POSTS if title in post['title'].casefold()
+                              and content in post['content'].casefold()]
+            return jsonify(filtered_posts)
+        elif title:
+            filtered_posts = [post for post in POSTS if title in post['title'].casefold()]
+            return jsonify(filtered_posts)
+        elif content:
+            filtered_posts  = [post for post in POSTS if content in post['content'].casefold()]
+            return jsonify(filtered_posts)
+        else:
+            return jsonify(POSTS)
+
+
+
+
+@app.route('/api/posts/<int:id>', methods=['DELETE'])
+def delete_post(id):
+    post = find_post_by_id(id)
+
+    if post is None:
+        return '', 404
+
+    global POSTS
+    new_posts = [post for post in POSTS if post['id'] != id]
+    POSTS = new_posts
+    return jsonify(post)
+
+
+@app.route('/api/posts/<int:id>', methods=['PUT'])
+def update_post(id):
+    post = find_post_by_id(id)
+
+    if post is None:
+        return '', 404
+
+    new_data = request.get_json()
+    post.update(new_data)
+
+    return jsonify(post)
+
+def find_post_by_id(post_id):
+    post = [post for post in POSTS if post['id'] == post_id]
+    if post:
+        return post[0]
+    else:
+        return None
+
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return jsonify({"error": "Not Found"}), 404
+
+
+@app.errorhandler(405)
+def method_not_allowed_error(error):
+    return jsonify({"error": "Method Not Allowed"}), 405
 
 
 if __name__ == '__main__':
