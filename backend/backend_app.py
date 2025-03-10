d42f137a7dea2d01dd4ee660e6b3288cc6e32d3e
malgorzata <libronika@gmail.com>
2025-03-10 12:21:52 +0100

    limiter added, variable names changed: POSTS > posts

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -4,43 +4,47 @@ from operator import itemgetter
 import helper
 from datetime import date as get_date
 from flask_swagger_ui import get_swaggerui_blueprint
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 
 app = Flask(__name__)
+limiter = Limiter(app=app, key_func=get_remote_address)
 CORS(app)  # This will enable CORS for all routes
 
 
-SWAGGER_URL="/api/docs"  # (1) swagger endpoint e.g. HTTP://localhost:5002/api/docs
-API_URL="/static/masterblog.json" # (2) ensure you create this dir and file
+SWAGGER_URL="/api/docs"  # swagger endpoint e.g. HTTP://localhost:5002/api/docs
+API_URL="/static/masterblog.json"
 
 swagger_ui_blueprint = get_swaggerui_blueprint(
     SWAGGER_URL,
     API_URL,
     config={
-        'app_name': 'Masterblog API' # (3) You can change this if you like
+        'app_name': 'Masterblog API'
     }
 )
 app.register_blueprint(swagger_ui_blueprint, url_prefix=SWAGGER_URL)
 
 @app.route('/api/posts', methods=['GET', 'POST'])
+@limiter.limit("10/minute")
 def get_posts():
     """
     Fetches all the posts from API,
     but allows the user to filter or sort them before returning.
     POST method allows the user to add a new post to the API.
     """
-    POSTS = helper.get_all_posts()
+    posts = helper.get_all_posts()
     if request.method == 'POST':
         new_post = request.get_json()
         if not helper.validate_post_data(new_post):
             return jsonify({"error": "Invalid post title, content or author"}), 400
-        if POSTS:
-            new_id = max(post['id'] for post in POSTS) + 1
+        if posts:
+            new_id = max(post['id'] for post in posts) + 1
         else:
             new_id = 1
         new_post['id'] = new_id
         new_post['date'] = str(get_date.today())
-        POSTS.append(new_post)
-        helper.save_all_posts_to_file(POSTS)
+        posts.append(new_post)
+        helper.save_all_posts_to_file(posts)
         return jsonify(new_post), 201
     else:
         title = request.args.get('title')
@@ -51,7 +55,7 @@ def get_posts():
         direction = request.args.get('direction')
 
         user_filters = {'title': title, 'content': content, 'author': author, 'date': date}
-        filtered_posts = POSTS
+        filtered_posts = posts
         if any(filter_content for filter_content in user_filters.values()):
             for filter_type, filter_content in user_filters.items():
                 if filter_content:
@@ -62,17 +66,17 @@ def get_posts():
         if sort or direction:
             if helper.validate_filters(sort, direction):
                 if sort and direction:
-                    sorted_posts = sorted(POSTS, key= lambda post: post[sort].casefold(), reverse=direction == 'desc')
+                    sorted_posts = sorted(posts, key= lambda post: post[sort].casefold(), reverse=direction == 'desc')
                     return jsonify(sorted_posts)
                 elif sort:
-                    sorted_posts = sorted(POSTS, key=lambda post: post[sort].casefold())
+                    sorted_posts = sorted(posts, key=lambda post: post[sort].casefold())
                     return jsonify(sorted_posts)
                 elif direction:
-                    sorted_posts = sorted(POSTS, key=itemgetter('id'), reverse=direction == 'desc')
+                    sorted_posts = sorted(posts, key=itemgetter('id'), reverse=direction == 'desc')
                     return jsonify(sorted_posts)
             else:
                 return jsonify({"error": "Bad Request"}), 400
-        return jsonify(POSTS)
+        return jsonify(posts)
 
 
 @app.route('/api/posts/<int:id>', methods=['DELETE'])
@@ -83,8 +87,8 @@ def delete_post(id):
     post = helper.find_post_by_id(id)
     if post is None:
         return '', 404
-    POSTS = helper.get_all_posts()
-    new_posts = [post for post in POSTS if post['id'] != id]
+    posts = helper.get_all_posts()
+    new_posts = [post for post in posts if post['id'] != id]
     helper.save_all_posts_to_file(new_posts)
     return jsonify(post)
 
@@ -99,13 +103,13 @@ def update_post(id):
         return '', 404
     new_data = request.get_json()
 
-    POSTS = helper.get_all_posts()
+    posts = helper.get_all_posts()
 
-    for post in POSTS:
+    for post in posts:
         if post["id"] == id:
             post.update(new_data)
 
-    helper.save_all_posts_to_file(POSTS)
+    helper.save_all_posts_to_file(posts)
     return jsonify(post)
 
 
